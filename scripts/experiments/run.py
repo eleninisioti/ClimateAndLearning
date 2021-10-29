7629a85f64e69207894c82bf40cb340972ac384d
Eleni <e.nisioti@essex.ac.uk>
2021-10-29 14:04:14 +0200

    small fix

--- scripts/experiments/run.py
+++ scripts/experiments/run.py
@@ -375,21 +375,22 @@ def parametric_variable(gpu, trial,  mode, long_run=False):
     top_dir = "Maslin/1D_mutate/parametric_variable/"
     experiments = []
     param_names = ["--project", "--env_type", "--model", "--num_gens", "--trial", "--factor_time_abrupt",
-                   "--factor_time_variable", "--mutate_rate", "--var_freq", "--var_SD"]
+                   "--factor_time_variable", "--mutate_rate", "--var_freq", "--var_SD", "--factor_time_steady"]
     env_type = "combined"
     model = "hybrid"
-    num_gens = 1000
+    num_gens = 2000
     factor_time_abrupt = 2
     mutate_rate = 0.001
     var_freq_values = np.arange(1, 100, 20)
     var_SD_values = [0.05, 0.1, 0.2, 0.4]
     factor_time_variable = 1
+    factor_time_steady = 0.2
 
     for var_SD in var_SD_values:
         for var_freq in var_freq_values:
             project = top_dir + "SD_" + str(var_SD) + "_var_" + str(var_freq)
             new_exp = [project, env_type, model, num_gens, trial,  factor_time_abrupt, factor_time_variable,
-                       mutate_rate, var_SD, var_freq]
+                       mutate_rate, var_SD, var_freq, factor_time_steady]
             experiments.append(new_exp)
             if mode == "local":
                 command = "python simulate.py "
@@ -424,4 +425,4 @@ if __name__ == "__main__":
         #exp_tune_var6_irreg(gpu=False, trial=trial, mode=mode)
         #debug(gpu=False, trial=trial, mode=mode)\
         #exp_parametric(gpu=True, trial=trial, mode=mode)
-        parametric_abrupt(gpu=True, trial=trial, mode=mode)
+        parametric_variable(gpu=True, trial=trial, mode=mode)

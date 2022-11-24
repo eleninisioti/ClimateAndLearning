ed74cdea9ad28ed7ab5dd9f101dca8426bad4ddd
Eleni <e.nisioti@essex.ac.uk>
2022-11-24 13:59:59 +0100

    tracking niche population

---
+++ jz_scripts/24_11_2022/climate_sin_select_NF.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J fully
+#SBATCH -t 30:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=/scratch/enisioti/climate_log/jz_logs/%j.out
+#SBATCH --error=/scratch/enisioti/climate_log/jz_logs/%j.err
+module load pytorch-gpu/py3/1.7.1
+python simulate.py --project ../projects//24_11_2022/manim_fig8/S_NF_G_evolv_N_100_climate_0.2_T_46_A_0.2 --env_type sin --num_gens 500 --trial 0 --selection_type NF --genome_type evolv --num_niches 100 --climate_mean_init 0.2 --amplitude 0.2 --period 46 

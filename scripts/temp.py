c2a34e474f00ed418200e3e457e1a65594c03f18
Eleni nisioti <eleninisioti@elenis-mbp.home>
2023-01-03 13:14:29 +0100

    move

---
+++ scripts/temp.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+
+
+or_project_dir = "/scratch/enisioti/climate_log/projects/niche_construction/27_12_2022/noisy"
+projects = [os.path.join(or_project_dir, o) for o in os.listdir(or_project_dir)]
+projects_cut = os.listdir(or_project_dir)
+for project in projects:
+    trials = [os.path.join(project, o) for o in os.listdir(project + "/trials")]
+    for file in trials:
+        new_dir = "/scratch/enisioti/climate_log/projects/niche_construction/30_12_2022/noisy/trials"
+        print(file, new_dir)
+        #shutil(file, new_dir)
+

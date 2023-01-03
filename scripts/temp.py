666f90eb9eb8c81750aff42246767e7084901eaa
Eleni nisioti <eleninisioti@elenis-mbp.home>
2023-01-03 13:17:46 +0100

    move

--- scripts/temp.py
+++ scripts/temp.py
@@ -4,7 +4,8 @@ import shutil
 
 or_project_dir = "/scratch/enisioti/climate_log/projects/niche_construction/27_12_2022/noisy"
 projects = [os.path.join(or_project_dir, o) for o in os.listdir(or_project_dir)]
-projects_cut = os.listdir(or_project_dir)
+projects = [el for el in projects if "plots" not in el]
+
 for project in projects:
     trials = [os.path.join(project, o) for o in os.listdir(project + "/trials") if "plots" not in o]
     for file in trials:
